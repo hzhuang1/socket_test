1583939205a862b11f21acef8db13daadb8aae00
Haojian Zhuang <haojian.zhuang@gmail.com>
2024-06-21 15:51:43 +0800

    merge start_ssl() and start_ktls() together in perf_test
    Merge start_ssl() and start_ktls() together since there's only a
    little difference in it.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@gmail.com>

--- perf_test.c
+++ perf_test.c
@@ -29,13 +29,13 @@
 #define NSECS_IN_SEC	(1000000000)
 #define MSECS_IN_SEC	(1000000)
 
-#define TIMER_INTERVAL	2
-//#define TIMER_INTERVAL	180
+//#define TIMER_INTERVAL	2
+#define TIMER_INTERVAL	10
 
 #define CERTIFICATE_FILE "server.crt"
 #define PRIVATE_KEY_FILE "server.key"
 
-#define ONLY_ONE_MESSAGE	1	// one data message
+//#define ONLY_ONE_MESSAGE	1	// one data message
 
 typedef void (*client_t)(int sockfd, int fd, void *src, size_t len);
 typedef void (*server_t)(int sockfd, void *dst, size_t len);
@@ -52,6 +52,7 @@ typedef struct {
 
 static int stop_flag = 0;
 static int msg_count = 0;
+static int ssl_ktls_flag = 0;
 
 static int stop_test(int sockfd, struct timespec start)
 {
@@ -310,7 +311,6 @@ void client_ssl_send_fdata(SSL *ssl, int fd, void *src, size_t len)
 
 	ret = SSL_write(ssl, src, sizeof(msg_t));
 	if (ret < (int)sizeof(msg_t)) {
-		printf("ret:%d, msg_t:%d\n", ret, sizeof(msg_t));
 		perror("send hdr");
 		exit(EXIT_FAILURE);
 	}
@@ -329,19 +329,14 @@ void client_ssl_send_fdata(SSL *ssl, int fd, void *src, size_t len)
 
 void client_ssl_send_file(SSL *ssl, int fd, void *src, size_t len)
 {
-	size_t res;
 	int ret;
 
 	ret = SSL_write(ssl, src, sizeof(msg_t));
 	if (ret < (int)sizeof(msg_t)) {
-		printf("ret:%d, msg_t:%d\n", ret, sizeof(msg_t));
 		perror("send hdr");
 		exit(EXIT_FAILURE);
 	}
 	lseek(fd, 0, SEEK_SET);
-	printf("ktls send:%ld, ktls recv:%ld\n",
-		BIO_get_ktls_send(SSL_get_wbio(ssl)),
-		BIO_get_ktls_recv(SSL_get_rbio(ssl)));
 	ret = SSL_sendfile(ssl, fd, 0, len, 0);
 	if (ret < (int)len) {
 		printf("ret:%d\n", ret);
@@ -450,6 +445,7 @@ static void start_sock(client_t client_send, server_t server_recv, int file_fd,
 			exit(EXIT_FAILURE);
 		}
 
+		memset(src, 0, len + sizeof(msg_t));
 		set_message_header(src, len, msg_count);
 		/*
 		printf("src:\n");
@@ -492,6 +488,7 @@ static void start_sock(client_t client_send, server_t server_recv, int file_fd,
 		(void)diff;
 #else
 		while (1) {
+			memset(dst, 0, len + sizeof(msg_t));
 			server_recv(sockfd, dst, len);
 
 			msg = (msg_t *)dst;
@@ -572,6 +569,8 @@ static void start_ssl(client_ssl_t client_send, server_ssl_t server_recv,
 
 		ssl = SSL_new(ctx);
 		SSL_set_fd(ssl, fd[0]);		// bind ssl with sock
+		if (ssl_ktls_flag)
+			SSL_set_options(ssl, SSL_OP_ENABLE_KTLS);
 
 		if (SSL_connect(ssl) == -1) {
 			perror("ssl connect");
@@ -584,11 +583,16 @@ static void start_ssl(client_ssl_t client_send, server_ssl_t server_recv,
 			exit(EXIT_FAILURE);
 		}
 
+		printf("ktls send:%ld, ktls recv:%ld\n",
+			BIO_get_ktls_send(SSL_get_wbio(ssl)),
+			BIO_get_ktls_recv(SSL_get_rbio(ssl)));
+
 		if (start_timer(&start) < 0) {
 			close(fd[0]);
 			exit(EXIT_FAILURE);
 		}
 
+		memset(src, 0, len + sizeof(msg_t));
 		set_message_header(src, len, msg_count);
 #ifdef ONLY_ONE_MESSAGE
 		client_send(ssl, file_fd, src, len);
@@ -668,225 +672,7 @@ static void start_ssl(client_ssl_t client_send, server_ssl_t server_recv,
 		(void)diff;
 #else
 		while (1) {
-			server_recv(ssl, dst, len);
-
-			msg = (msg_t *)dst;
-			if (memcmp(msg->cmd, STOP_MSG, MSG_HDR_LEN) == 0) {
-				if (cnt > msg->count) {
-					perror("wrong count");
-					exit(EXIT_FAILURE);
-				} else if (cnt == msg->count)
-					break;
-			}
-			cnt++;
-		}
-
-		// msg->count is counted from 0. So stop message is not
-		// recorded in msg->count.
-		if (cnt == msg->count) {
-			get_time_end(&end);
-			memcpy(&start, &msg->start, sizeof(start));
-			get_time_diff(start, end, &diff);
-			// At here, stop message is received.
-			// msg->length indicates the length of stop message,
-			// not data message.
-			// Only calculate data payload size.
-			calc_perf(diff, msg->count, len);
-		}
-#endif
-		SSL_shutdown(ssl);
-		SSL_free(ssl);
-		SSL_CTX_free(ctx);
-
-		close(sockfd);
-		close(fd[1]);
-		exit(0);
-	}
-}
-
-static void start_ktls(client_ssl_t client_send, server_ssl_t server_recv,
-		       int file_fd, void *src, void *dst, size_t len)
-{
-	int fd[2];
-	int sockfd;
-	pid_t pid;
-	struct timespec start, end, diff;
-	int cnt = 0;
-	msg_t *msg;
-
-	fd[1] = server_socket();
-	pid = fork();
-	if (pid < 0) {
-		perror("fork");
-		exit(1);
-	}
-
-	if (pid) {
-		// parent
-		SSL_CTX *ctx;
-		SSL *ssl;
-		long verify_result;
-		struct tls_crypto_info_keys tls12;
-		int opt = 1;
-		int ret;
-
-		SSL_library_init();
-		OpenSSL_add_all_algorithms();
-		SSL_load_error_strings();
-
-		ctx = SSL_CTX_new(TLSv1_2_client_method());
-		if (ctx == NULL) {
-			perror("ctx");
-			exit(EXIT_FAILURE);
-		}
-		if (SSL_CTX_load_verify_locations(ctx, CERTIFICATE_FILE, NULL)
-		    != 1) {
-			perror("CA failed");
-			exit(EXIT_FAILURE);
-		}
-		SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
-
-		fd[0] = client_socket();	// create client ssl
-		if (setsockopt(fd[0], SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-			perror("setsockopt");
-			exit(EXIT_FAILURE);
-		}
-
-		/*
-		tls_crypto_info_init(TLS_1_2_VERSION, TLS_CIPHER_AES_GCM_128,
-				     &tls12);
-		// enable TLS
-		ret = setsockopt(fd[0], SOL_TCP, TCP_ULP, "tls", sizeof("tls"));
-		if (ret < 0) {
-			perror("set TCP_ULP\n");
-			exit(EXIT_FAILURE);
-		}
-		// start TLS
-		ret = setsockopt(fd[0], SOL_TLS, TLS_TX, &tls12, tls12.len);
-		if (ret < 0) {
-			perror("set TLS_TX");
-			exit(EXIT_FAILURE);
-		}
-		*/
-
-		ssl = SSL_new(ctx);
-		SSL_set_fd(ssl, fd[0]);		// bind ssl with sock
-		SSL_set_options(ssl, SSL_OP_ENABLE_KTLS);
-
-		if (SSL_connect(ssl) == -1) {
-			perror("ssl connect");
-			exit(EXIT_FAILURE);
-		}
-
-		verify_result = SSL_get_verify_result(ssl);
-		if (verify_result != X509_V_OK) {
-			perror("SSL failed");
-			exit(EXIT_FAILURE);
-		}
-
-		if (start_timer(&start) < 0) {
-			close(fd[0]);
-			exit(EXIT_FAILURE);
-		}
-
-		set_message_header(src, len, msg_count);
-#ifdef ONLY_ONE_MESSAGE
-		client_send(ssl, file_fd, src, len);
-		msg_count++;
-#else
-		do {
-			client_send(ssl, file_fd, src, len);
-			msg_count++;
-		} while (stop_flag == 0);
-#endif
-		stop_ssl_test(ssl, start);
-
-		SSL_shutdown(ssl);
-		SSL_free(ssl);
-		SSL_CTX_free(ctx);
-
-		close(fd[0]);
-		waitpid(-1, NULL, 0);
-	} else {
-		// child
-		SSL_CTX *ctx;
-		SSL *ssl;
-		struct tls_crypto_info_keys tls12;
-		int ret;
-
-		OpenSSL_add_all_algorithms();
-		SSL_load_error_strings();
-
-		ctx = SSL_CTX_new(TLSv1_2_server_method());
-		if (ctx == NULL) {
-			perror("ssl ctx");
-			exit(EXIT_FAILURE);
-		}
-
-		if (SSL_CTX_use_certificate_file(ctx, CERTIFICATE_FILE,
-						 SSL_FILETYPE_PEM) <= 0) {
-			perror("ssl cert");
-			exit(EXIT_FAILURE);
-		}
-
-		if (SSL_CTX_use_PrivateKey_file(ctx, PRIVATE_KEY_FILE,
-						SSL_FILETYPE_PEM) <= 0) {
-			perror("ssl key");
-			exit(EXIT_FAILURE);
-		}
-
-		if (!SSL_CTX_check_private_key(ctx)) {
-			fprintf(stderr, "Private key does not match the public certificate\n");
-			exit(EXIT_FAILURE);
-		}
-
-		/*
-		tls_crypto_info_init(TLS_1_2_VERSION, TLS_CIPHER_AES_GCM_128,
-				     &tls12);
-		// enable TLS
-		ret = setsockopt(fd[0], SOL_TCP, TCP_ULP, "tls", sizeof("tls"));
-		if (ret < 0) {
-			perror("set TCP_ULP\n");
-			exit(EXIT_FAILURE);
-		}
-		// start TLS
-		ret = setsockopt(fd[0], SOL_TLS, TLS_TX, &tls12, tls12.len);
-		if (ret < 0) {
-			perror("set TLS_TX");
-			exit(EXIT_FAILURE);
-		}
-		*/
-
-		sockfd = accept(fd[1], NULL, NULL);
-		if (sockfd < 0) {
-			perror("accept");
-			return;
-		}
-
-		ssl = SSL_new(ctx);
-		SSL_set_fd(ssl, sockfd);
-
-		if (SSL_accept(ssl) <= 0) {
-			perror("ssl accept");
-			exit(EXIT_FAILURE);
-		}
-
-#ifdef ONLY_ONE_MESSAGE
-		server_recv(ssl, dst, len);
-		printf("dst:\n");
-		dump_buffer_hex(dst, len + sizeof(msg_t));
-
-		msg = (msg_t *)dst;
-		if (memcmp(msg->cmd, STOP_MSG, MSG_HDR_LEN) == 0) {
-			if (cnt > msg->count) {
-				perror("wrong count");
-				exit(EXIT_FAILURE);
-			}
-		}
-		(void)end;
-		(void)diff;
-#else
-		while (1) {
+			memset(dst, 0, len + sizeof(msg_t));
 			server_recv(ssl, dst, len);
 
 			msg = (msg_t *)dst;
@@ -1107,6 +893,7 @@ int do_ssl_file(char *name)
 	int ret;
 	struct stat st;
 
+	ssl_ktls_flag = 1;
 	fd = open(name, O_RDONLY);
 	if (fd < 0) {
 		perror("open");
@@ -1134,7 +921,7 @@ int do_ssl_file(char *name)
 		goto out_dst;
 	}
 	memset(dst, 0, msg_sz);
-	start_ktls(client_ssl_send_file, server_ssl_recv, fd, src, dst, len);
+	start_ssl(client_ssl_send_file, server_ssl_recv, fd, src, dst, len);
 	close(fd);
 	free(dst);
 	free(src);
@@ -1147,10 +934,10 @@ out:
 
 int main(void)
 {
-	//do_sock_send(4096);
-	//do_sock_fdata("data_64k.bin");
-	//do_sock_file("data_128.bin");
-	//do_ssl_fdata("data_128.bin");
-	do_ssl_file("data_128.bin");
+	//do_sock_send(128);
+	//do_sock_fdata("data_2m.bin");
+	do_sock_file("data_128.bin");
+	//do_ssl_fdata("data_2m.bin");
+	//do_ssl_file("data_2m.bin");
 	return 0;
 }
